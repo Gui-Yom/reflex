22981bc6b0bc8df5482e1576d565ac50feb4ac6c
ArthurWauquier <arthur.wauquier@insa-lyon.fr>
2021-03-31 14:17:13 +0200

    Update Segment.java

--- src/Segment.java
+++ src/Segment.java
@@ -1,10 +1,12 @@
 public class Segment {
 	protected Vec2f pointA;
 	protected Vec2f pointB;
+	protected Vec2f normale;
 	
 	public Segment (Vec2f a, Vec2f b){
 		this.pointA = a;
 		this.pointB = b;
+		this.normale = this.getNormale();
 	}
 	
 	public Vec2f getNormale(){
