36522aa9768b9aaf4990610a537f47b5ac756e2d
Guillaume Anthouard <guillaume.anthouard@hotmail.fr>
2021-05-05 14:30:57 +0200

    Swap panels instead of creating a new window

--- src/FenetreInformation.java
+++ src/FenetreInformation.java
@@ -8,7 +8,8 @@ import java.io.IOException;
 
 public class FenetreInformation extends JFrame {
 
-    private JFrame winExplications;
+    private JPanel panelPresentation;
+    private JPanel panelExplications;
     private JButton btnCompris;
     private JButton btnExplications;
 
@@ -20,8 +21,8 @@ public class FenetreInformation extends JFrame {
         setResizable(false);
         setLayout(null);
 
-        JPanel panFond = new JPanel();
-        panFond.setBounds(0, 0, getWidth(), getHeight());
+        panelPresentation = new JPanel();
+        panelPresentation.setBounds(0, 0, getWidth(), getHeight());
 
         //paramètre du bouton
 
@@ -32,7 +33,6 @@ public class FenetreInformation extends JFrame {
         btnExplications.setForeground(new Color(243, 135, 37));
         btnExplications.setFont(police);
         btnExplications.setBounds(getWidth() / 4, 50, getWidth() / 2, 40);
-        btnExplications.addActionListener(e -> winExplications.setVisible(true));
 
         //image de fond
         ImageIcon imageFond = null;
@@ -47,28 +47,25 @@ public class FenetreInformation extends JFrame {
         JLabel fond = new JLabel(imageFond);
         fond.setBounds(0, 0, getWidth(), getHeight());
         fond.add(btnExplications);
-        panFond.add(fond);
+        panelPresentation.add(fond);
 
-        setContentPane(panFond);
-        setVisible(true);
-
-        winExplications = new JFrame("Explications");
-        winExplications.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        winExplications.setBounds(1000, 50, 350, 500);
-        winExplications.setLayout(null);
-
-        JPanel expFond = new JPanel();
-        expFond.setBounds(0, 0, winExplications.getWidth(), winExplications.getHeight());
-        expFond.setBackground(new Color(16, 31, 64));
+        // Panel d'explications
+        panelExplications = new JPanel();
+        panelExplications.setBounds(0, 0, getWidth(), getHeight());
+        panelExplications.setBackground(new Color(16, 31, 64));
 
         btnCompris = new JButton("Compris !");
         btnCompris.setBackground(new Color(64, 76, 98));
         btnCompris.setForeground(new Color(243, 135, 37));
         btnCompris.setFont(police);
         btnCompris.setBounds(250, 820, 150, 40);
-        btnCompris.addActionListener(e -> winExplications.setVisible(false));
 
-        expFond.add(btnCompris);
-        winExplications.add(expFond);
+        panelExplications.add(btnCompris);
+
+        btnExplications.addActionListener(e -> setContentPane(panelExplications));
+        btnCompris.addActionListener(e -> setContentPane(panelPresentation));
+
+        setContentPane(panelPresentation);
+        setVisible(true);
     }
 }
